c0007a312f0de61a80d281f2758c06e354da960b
Andy Tang <kiwikefei@gmail.com>
2017-06-22 09:32:08 +1200

    simplified chinese version translation

---
+++ resources/lang/cn/adminlte.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'full_name'                   => '姓名',
+    'email'                       => '电子邮件',
+    'password'                    => '密码',
+    'retype_password'             => '重新输入密码',
+    'remember_me'                 => '记住我',
+    'register'                    => '注册',
+    'register_a_new_membership'   => '注册新用户',
+    'i_forgot_my_password'        => '忘记密码',
+    'i_already_have_a_membership' => '已经是会员',
+    'sign_in'                     => '登入',
+    'log_out'                     => '退出',
+    'toggle_navigation'           => '导航开关',
+    'login_message'               => '请登入',
+    'register_message'            => '注册新用户',
+    'password_reset_message'      => '密码重置',
+    'reset_password'              => '密码重置',
+    'send_password_reset_link'    => '发送密码重置邮件',
+];
